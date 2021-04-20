b332edf04cd692cbf8bcc83584ac501a7ba1e895
Manish Jindal <manish.kumar.jindal@mendix.com>
2021-04-20 16:57:12 +0530

    Support for fetching STS token from TVM for mendix runtime version > 9.2

--- buildpack/runtime_components/storage.py
+++ buildpack/runtime_components/storage.py
@@ -7,7 +7,7 @@ from buildpack import util
 
 
 def _get_s3_specific_config(vcap_services, m2ee):
-    access_key = secret = bucket = encryption_keys = key_suffix = None
+    access_key = secret = tvm_endpoint= tvm_username = tvm_password = bucket = encryption_keys = key_suffix = None
     endpoint = None
     v2_auth = ""
     amazon_s3 = None
@@ -23,9 +23,17 @@ def _get_s3_specific_config(vcap_services, m2ee):
 
     if amazon_s3:
         _conf = vcap_services[amazon_s3][0]["credentials"]
-        access_key = _conf["access_key_id"]
-        secret = _conf["secret_access_key"]
         bucket = _conf["bucket"]  # see below at hacky for actual conf
+        if "access_key_id" in _conf:
+            access_key = _conf["access_key_id"]
+        if "secret_access_key" in _conf:
+            secret = _conf["secret_access_key"]
+        if "tvm_endpoint" in _conf:
+            tvm_endpoint = _conf["tvm_endpoint"]
+        if "tvm_username" in _conf:
+            tvm_username = _conf["tvm_username"]
+        if "tvm_password" in _conf:
+            tvm_password = _conf["tvm_password"]
         if "encryption_keys" in _conf:
             encryption_keys = _conf["encryption_keys"]
         if "key_suffix" in _conf:
@@ -53,6 +61,9 @@ def _get_s3_specific_config(vcap_services, m2ee):
 
     access_key = os.getenv("S3_ACCESS_KEY_ID", access_key)
     secret = os.getenv("S3_SECRET_ACCESS_KEY", secret)
+    tvm_endpoint = os.getenv("S3_TVM_ENDPOINT", tvm_endpoint)
+    tvm_username = os.getenv("S3_TVM_USERNAME", tvm_username)
+    tvm_password = os.getenv("S3_TVM_PASSWORD", tvm_password)
     bucket = os.getenv("S3_BUCKET_NAME", bucket)
     if "S3_ENCRYPTION_KEYS" in os.environ:
         encryption_keys = json.loads(os.getenv("S3_ENCRYPTION_KEYS"))
@@ -65,16 +76,54 @@ def _get_s3_specific_config(vcap_services, m2ee):
     v2_auth = os.getenv("S3_USE_V2_AUTH", v2_auth).lower() == "true"
     sse = os.getenv("S3_USE_SSE", "").lower() == "true"
 
-    if not (access_key and secret and bucket):
-        return None
+    if (access_key and secret and bucket):
+        logging.info("S3 config detected, activating external file store")
+        config = {
+            "com.mendix.core.StorageService": "com.mendix.storage.s3",
+            "com.mendix.storage.s3.AccessKeyId": access_key,
+            "com.mendix.storage.s3.SecretAccessKey": secret,
+            "com.mendix.storage.s3.BucketName": bucket,
+        }
+    elif (tvm_endpoint and tvm_username and tvm_password and bucket):
+        if m2ee.config.get_runtime_version() < 9.2:
+            logging.info("S3 config detected, fetching IAM credentials from TVM")
+            response = requests.get(
+                "https://%s/v1/getcredentials" % tvm_endpoint,
+                headers={
+                "User-Agent": "Mendix Buildpack",
+                "Authorization": _get_credential(tvm_username, tvm_password),
+                }
+            )
+            if not response.ok:
+                logging.error("Failed to get IAM credential from TVM")
+                return None
+            result = response.json()
 
-    logging.info("S3 config detected, activating external file store")
-    config = {
-        "com.mendix.core.StorageService": "com.mendix.storage.s3",
-        "com.mendix.storage.s3.AccessKeyId": access_key,
-        "com.mendix.storage.s3.SecretAccessKey": secret,
-        "com.mendix.storage.s3.BucketName": bucket,
-    }
+            if "AccessKeyID" in result:
+                access_key = result["AccessKeyID"]
+            if "SecretAccessKey" in result:
+                secret = result["SecretAccessKey"]
+
+            if (access_key and secret):
+                config = {
+                    "com.mendix.core.StorageService": "com.mendix.storage.s3",
+                    "com.mendix.storage.s3.AccessKeyId": access_key,
+                    "com.mendix.storage.s3.SecretAccessKey": secret,
+                    "com.mendix.storage.s3.BucketName": bucket,
+                }
+            else:
+                logging.error("Failed to get AccessKeyID or SecretAccessKey from TVM")
+                return None
+        else:
+            logging.info("S3 config detected, activating external file store")
+            config = {
+                "com.mendix.core.StorageService": "com.mendix.storage.s3",
+                "com.mendix.storage.s3.tokenService.Url" : "https://%s/v1/gettoken" % tvm_endpoint,
+                "com.mendix.storage.s3.tokenService.Username" : tvm_username,
+                "com.mendix.storage.s3.tokenService.Password" : tvm_password,
+            }
+    else:
+        return None
 
     if dont_perform_deletes:
         logging.debug("disabling perform deletes for runtime")
@@ -181,6 +230,10 @@ def _get_azure_storage_specific_config(vcap_services, m2ee):
     else:
         return None
 
+def _get_credential(username, password):
+    userpass = username + ':' + password
+    encoded_userpass = base64.b64encode(userpass.encode()).decode()
+    return "Basic %s" % encoded_userpass
 
 def get_config(m2ee):
     vcap_services = util.get_vcap_services_data()
